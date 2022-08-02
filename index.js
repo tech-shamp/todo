69a999203f1295aa3ec90236af82df3cad57999c
tech-shamp <shampytechnical@gmail.com>
2022-08-03 03:51:09 +0500

    added fonts written basic css

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(dir_path));
 
-app.get("/", (req, res) => document.write(dir_path));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(dir_path, "index.html"));
+});
 
 app.listen(port, console.log(`listening....... on ${port}`));
