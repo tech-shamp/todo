5e0945f528ac050a2de653f37a31d3313fa1e961
tech-shamp <shampytechnical@gmail.com>
2022-08-06 13:08:27 +0500

    Seperated Each file as components and use them anywhere i want. Now i can work on my project more managed

---
+++ multiply.js
@@ -0,0 +1,14 @@
+let Input = [
+  Name = 'Technical Shampy',
+  age = 16,
+  Class = 'I.C.S (Phy)',
+  subjects = ['computer science', 'mathamatics', 'physics']
+]
+
+Input += Input[3].includes('English')
+
+console.log(Input[0])
+console.log(Input[1])
+console.log(Input[2])
+console.log(Input[3])
+
---
+++ public/app.js
@@ -0,0 +1,2 @@
+
+// button.addEventListener("click", F2C)
