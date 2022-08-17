3570beb090bb20ea0a53df4ca946744f3414e47b
tech-shamp <shampytechnical@gmail.com>
2022-08-18 03:03:42 +0500

    server

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 const app = express();
 const dir_path = path.join(__dirname, "public");
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 999;
 
 app.use(express.static(dir_path));
 
@@ -11,8 +11,8 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(dir_path, "index.html"));
 });
 
-app.get((req, res) => {
-  res.sendFile(path.join(dir_path, "index.html"));
+app.get("*", (req, res) => {
+  res.status(404).sendFile(path.join(dir_path, "index.html"));
 });
 
 app.listen(port, console.log(`server listening on ${port}`));
