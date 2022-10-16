986ff7692416345af9e1ac4499b5a43e9cfb6ffc
tech-shamp <shampytechnical@gmail.com>
2022-10-16 07:10:18 +0500

    minor changes

--- public/app.js
+++ public/app.js
@@ -20,3 +20,25 @@ I just have to modify some little changes in that module */
 F2C(32); // This Function is Imported from components/c2f.js
 /* Farenhiet to Celcius is working Perfect
 I just have to modify some little changes in that module */
+
+
+console.info("Modern React Responsive Website")
+
+// Allow Push Notifications on Browser
+window.addEventListener("load", () => {
+  Notification.requestPermission().then((perm) => {
+    if (perm === "granted") {
+      console.info(`Permission Request ${perm}`)
+    } else console.error(`Permission Request ${perm}`)
+  })
+})
+
+const notif = new Notification("Modern React Responsive Website", {
+  body: "Modern React Responsive Website Created with React JS Technology by Meta from Facebook.",
+  icon: "img/bubble.png",
+  vibrate: true,
+})
+
+notif.addEventListener("error", (e) => {
+  alert(e)
+})
